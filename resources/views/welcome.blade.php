8aff96d094c9226820c9e0f6a6353b4c88ef640a
Deeva-Civia <s22210557@student.unklab.ac.id>
2026-04-21 13:31:05 +0800

    demo CI/CD

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -57,7 +57,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #003366;">
         <div class="container">
-            <a class="navbar-brand" href="/">MIS Registration</a>
+            <a class="navbar-brand" href="/">Deeva Civia Aulia Lolong</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
